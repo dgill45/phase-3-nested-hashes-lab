049a10e116981e0ef92ea026d3d403a61a987d9b
donellgill <donell.gill@outlook.com>
2021-07-04 19:41:16 -0400

    done

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+    programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +59,7 @@ def dennis_ritchies_language
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -82,6 +86,10 @@ def adding_matz
       }
     }
 
+    programmer_hash[:yukihiro_matsumoto => {}],
+    programmer_hash[:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}]
+    programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +112,10 @@ def changing_alan
       }
     }
 
+    alans_new_info = "GUI"
+
+programmer_hash[:alan_kay][:known_for] =  alans_new_info
+
 end
 
 def adding_to_dennis
@@ -125,5 +137,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 
 end
